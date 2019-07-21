9bbeb77cea4bc131acf5e4d2ed93119546729239
zhu <xxx@163.com>
2019-07-21 13:38:52 +0800

    zyz拉取

--- eason/src/components/ScienceBodyRouter/ScienceCom/Topic.vue
+++ eason/src/components/ScienceBodyRouter/ScienceCom/Topic.vue
@@ -1,29 +1,30 @@
 <template>
     <div>
-        <div v-for="(v,i) in arr" :key="i" @click="topicid(v.id)">
-            <div>
-                <img :src="v.imgurl">
-                <div>
+        <div v-for="(v,i) in arr" :key="i" @click="topicid(v.id)" class="easons">
+            <div class="photoBig">
+                <img :src="v.imgurl" class="photo">
+                <div class="photoBigRigth">
                     <div>
-                        <span>{{v.name}}</span>
-                        <span>{{v.desk}}</span>
-                        <span>{{v.position}}</span>
+                        <span class="photoBigRigthTopa">{{v.name}}</span>
+                        <span class="photoBigRigthTopb">{{v.desk}}</span>
+                        <span class="photoBigRigthTopc">{{v.position}}</span>
                     </div>
                     <div>
-                        <span>原创</span>
-                        <span>{{v.time}}</span>
+                        <span class="photoBigRigthbottoma">原创</span>
+                        <span class="photoBigRigthbottomb">{{v.time}}</span>
                     </div>
                 </div>
             </div>
+
             <div>
-                <p>{{v.title}}</p>
+                <p class="photoTitle">{{v.title}}</p>
             </div>
             <div>
-                <span>{{v.click.content|s}}...</span>
+                <span class="photoContent">{{v.click.content|s}}...</span>
             </div>
             <div>
-                <span>{{v.readNumber}}</span>
-                <span>{{v.praiseNumber}}</span>
+                <span class="photoreadNumber">阅读{{v.readNumber}}</span>
+                <span class="photopraiseNumber">赞{{v.praiseNumber}}</span>
             </div>
         </div>
     </div>
@@ -36,7 +37,7 @@ export default {
     },
     filters:{
         s(val){
-            return val.slice(0,44)
+            return val.slice(0,40)
         }
     },
     methods: {
@@ -48,7 +49,61 @@ export default {
 </script>
 
 <style scoped>
-    
+    .easons{
+        padding:10px 15px;
+        border-bottom:1px solid #e7e7e7;
+    }
+    .photoBig{
+        display: flex;
+    }
+    .photo{
+        width:40px;
+        height:40px;
+        margin-right:14px;
+    }
+    .photoBigRigthTopa{
+        font-size:14px;
+        color:#323232;
+    }
+    .photoBigRigthTopb{
+        color:#666666;
+        font-size:12px;
+    }
+    .photoBigRigthTopc{
+        color:#999999;
+        font-size:12px;
+    }
+    .photoBigRigthbottoma{
+        color:#999999;
+        font-size:12px;
+        border:1px solid #999999;
+        padding:0 5px;
+        border-radius: 10px;
+    }
+    .photoBigRigthbottomb{
+        color:#999999;
+        font-size:12px;
+
+    }
+    .photoTitle{
+        font-size:16px;
+        font-weight: 600;
+        margin:10px 0;
+    }
+    .photoContent{
+        color:#999999;
+        font-size:13px;
+        line-height:20px;
+    }
+    .photoreadNumber{
+        font-size:12px;
+        color:#666666;
+        margin-right:8px;
+    }
+    .photopraiseNumber{
+        font-size:12px;
+        color:#666666;
+    }
 </style>
 
 
--- eason/src/components/ScienceDetails.vue
+++ eason/src/components/ScienceDetails.vue
@@ -4,46 +4,57 @@
             <div @click="routerGo()">
                 <span><img src="../../static/w/a61.png"></span>
             </div>
-            <div>
-                <span class="ScienceDetailsRight"><img src="../../static/w/asj.png"><span class="ScienceDetailsRightone">{{arr.click.collectNumber}}</span></span>
-                <span ><img src="../../static/w/a7r.png"><span class="ScienceDetailsRightone">{{arr.click.transmitNumber}}</span></span>
+            <div class="ScienceDetailsRightBig">
+                <div class="ScienceDetailsRightone ScienceDetailsRight"><img src="../../static/w/asj.png"><p>{{arr.click.collectNumber}}</p></div>
+                <div class="ScienceDetailsRightone"><img src="../../static/w/a7r.png"><p>{{arr.click.transmitNumber}}</p></div>
             </div>
         </div>
-
-        <div>
             <img class="ScienceDetailsTitleImg" :src="arr.imgurl">
+        <div class="ScienceDetailsBody">
+            
             <h3>{{arr.title}}</h3>
-            <div>
-                <img :src="arr.click.logurl">
-                <div>
-                    <div>
-                        <span>{{arr.click.title}}</span>
-                        <span>{{arr.click.subtitle}}</span>
+
+            <div class="public">
+                <img :src="arr.click.logurl" class="publicLeft">
+                <div class="publicRigth">
+                    <div class="publicRigthTop">
+                        <span class="publicRigthTopLeft">{{arr.click.title}}</span>
+                        <span class="publicRigthTopRight">{{arr.click.subtitle}}</span>
                     </div>
-                    <div>
+                    <div class="publicRigthBottom">
                         <span>{{arr.time}}</span>
                     </div>
                 </div>
             </div>
+
             <p>{{arr.click.content}}</p>
-            <p>图片来源:{{arr.click.mapDepot}}</p>
-            <img :src="arr.click.warrantyUrl">
-            <div>
-                <span>点赞{{arr.praiseNumber}}</span>
-                <span>立即咨询</span>
+            <p class="urlmapDepot">图片来源:{{arr.click.mapDepot}}</p>
+            <img :src="arr.click.warrantyUrl" class="warrantyUrl">
+            <div class="praiseNumber">
+                <span class="praiseNumberLeft">{{arr.praiseNumber}}</span>
+                <span  class="praiseNumberRight">立即咨询</span>
             </div>
 
             <div>
-                <span>分享到</span>
-                <div>
+                <div class="shareBig">
+                    <img src="../../static/w/ay2.png" class="share">
+                    <span class="sharetwo">分享到</span>
+                    <img src="../../static/w/ay2.png" class="share">
+                </div>
+                
+                <div class="sharepic">
                     <img src="../../static/w/arp.png">
                     <img src="../../static/w/arq.png">
                     <img src="../../static/w/arl.png">
                     <img src="../../static/w/aro.png">
                 </div>
-                <span v-for="(v,i) in arr.click.smallLabel" :key="i">{{v.title}}</span>
+                <span class="sharepiclist" v-for="(v,i) in arr.click.smallLabel" :key="i">{{v.title}}</span>
             </div>
         </div>
+        <div class="zhandifang">
+
+        </div>
+
         <div>
             <p class="ScienceDetailsFootP">猜你还想读</p>
             <div class="ScienceDetailsFoot">
@@ -74,8 +85,11 @@
                 </div>
             </div>
         </div>
+        <div class="youzhandifang">
+
+        </div>
         <div class="comment">
-            <input type="text" value="评论一下~">
+            <input class="commentInput" type="text" placeholder="评论一下~">
         </div>
     </div>
 </template>
@@ -105,14 +119,21 @@ export default {
         font-size:16px;
         color:#6bce72;
     }
+    .ScienceDetailsRightBig{
+        display: flex;
+    }
     .ScienceDetails img {
         width:22px;
         height:21px;
+        margin-right:5px;
     }
     .ScienceDetailsRight{
-        margin-right:25px;
+        margin-right:25px; 
     }
+    
     .ScienceDetailsRightone{
+        display: flex;
+        vertical-align:middle;
         line-height: 21px;
         color:black
     }
@@ -126,6 +147,9 @@ export default {
     }
     .ScienceDetailsFootP{
         border-bottom:1px solid #e7e7e7;
+        text-indent: 15px;
+        line-height: 42px;
+        color:#333333;
     }
     .ScienceDetailsFoot img{
         width:90px;
@@ -149,7 +173,148 @@ export default {
     .ScienceDetailsTitleImg{
         width:100%;
         height:160px;
+        margin-bottom:20px;
     }
-</style>
+    .ScienceDetailsBody{
+        padding:0 15px;
+    }
 
+    /* 公众号 */
+    .public{
+        display: flex;
+        margin:15px 0;
+    }
+    .publicLeft{
+        width:40px;
+        height:40px;
+        margin-right:10px;
+    }
+    .publicRigth{
+        font-size:14px;
+        display: flex;
+        flex-direction: column;
+        justify-content:space-between;
+        
+    }
+    .publicRigthTopRight{
+        border-radius:5px;
+        background:#ec706b;
+        color:#ffff;
+        padding:0 5px;
+        font-style:italic
+    }
+    .publicRigthBottom{
+        color:#999999;
+    }
+
+    .urlmapDepot{
+        line-height:27px;
+        font-size:16px;
+        margin:20px 0;
+        padding:0 10px;
+        border-left:5px solid #6cca7d;
+    }
+    /* 116 */
+    .warrantyUrl{
+        width:100%;
+        height:116px;
+        margin-bottom:37px;
+    }
+    .praiseNumber{
+        padding:22px 52px;
+    }
+    .praiseNumberLeft{
+        display: inline-block;
+        background:url(../../static/w/avc.png) no-repeat 15px 10px;
+        background-size:14px auto;
+        width:97px;
+        height:35px;
+        color:#bababa;
+        border-radius: 18px;
+        border:1px solid #bababa;
+        text-indent:40px;
+        margin-right:35px;
+        box-sizing: border-box;
+        font-size:12px;
+        line-height:35px;
+    }
+    /* b5q.9.png */
+    .praiseNumberRight{
+        display: inline-block;
+        background:url(../../static/w/ag4.png) no-repeat 15px 10px;
+        background-size:14px auto;
+        width:97px;
+        height:35px;
+        color:#75de7e;
+        border-radius: 18px;
+        border:1px solid #75de7e;
+        text-indent:32px;
+        box-sizing: border-box;
+        font-size:12px;
+        line-height:35px;
+    }
+    .share{
+        width:120px;
+        height:1px;
+        vertical-align:middle;
+    }
+    .sharetwo{
+        padding:0 24px;
+        line-height: 0;
+        vertical-align:middle;
+        color:#9b9b9b;
+        font-size:14px;
+    }
+    .sharepic{
+        display: flex;
+        justify-content: space-around;
+        padding:0 28px;
+        margin:30px 0;
+    }
+    .sharepic img{
+        width:45px;
+    }
+    .sharepiclist{
+        /* 116 48 */
+        padding:0 16px;
+        /* line-height: 28px; */
+        font-size: 14px;
+        color:#666666;
+        border:1px solid #e7e7e7;
+        margin-right:10px;
+        height:24px;
+        line-height: 24px;
+    }
+    .zhandifang{
+        width:100%;
+        height:10px;
+        background: #f3f3f3;
+        margin-top:15px;
+    }
+    .youzhandifang{
+        width:100%;
+        height:20px;
+        background: #f3f3f3;
+        border-bottom:1px solid #dedede;
+    }
+    .comment{
+        /* width:100%; */
+        padding:7px 15px;
+        background: #f0f0f0;
+        
+    }
+    .commentInput{
+        width:100%;
+        border-radius: 20px;;
+        border:1px solid #cdcdcd;
+        line-height: 28px;
+        text-indent: 15px;
+    }
+    .commentInput::-webkit-input-placeholder{
+         color:#7f7f7f; 
+        padding-left:15px;
+        
+    }
+    
+</style>
 
