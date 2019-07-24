def96f01a9fa9fee0c10cec9d09381f243927a3b
lijinghui <760995421@qq.com>
2019-07-24 12:19:01 +0800

    ljh3

--- eason/src/components/seekitem.vue
+++ eason/src/components/seekitem.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="search">
+         <img src="../../static/images/w/b1a.png" class="img1"  @click="fun()"/>
         <form action="/">
             <van-search
                 v-model="value"
@@ -13,14 +14,25 @@
 </template>
 
 <style scoped>
+    .search {
+        display: flex;
+        padding: 0px 10px;
+    }
+    .search .img1{
+        width:20px;
+        height: 20px;
+        margin:18px 6px 0px
+    }
     .van-field__control{
         background-color: #e9e9e9
     }
     .van-search__action{
         color: #747577;
     }
+    
     .van-search{
         padding: 10px 10px;
+        width:335px;
     }
   .van-search__content{
         border-radius: 30px;
@@ -38,7 +50,10 @@ export default {
    },
    methods: {
        onSearch(){},
-       onCancel(){}
+       onCancel(){},
+       fun(){
+            this.$router.go(-1) 
+       }
    },
 }
 </script>
